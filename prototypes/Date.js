7bcb4adbd53a576a96a924abede490535dc84786
Patrick <patrick.henninger@contagt.com>
2021-03-15 01:19:03 +0100

    Create Date.js

---
+++ prototypes/Date.js
@@ -0,0 +1,17 @@
+/* https://stackoverflow.com/questions/8083410/how-can-i-set-the-default-timezone-in-node-js */
+Date.prototype.toIsoString = function() {
+    var tzo = - this.getTimezoneOffset(),
+        dif = tzo >= 0 ? '+' : '-',
+        pad = function(num) {
+            var norm = Math.floor(Math.abs(num));
+            return (norm < 10 ? '0' : '') + norm;
+        };
+    return this.getFullYear() +
+        '-' + pad(this.getMonth() + 1) +
+        '-' + pad(this.getDate()) +
+        'T' + pad(this.getHours()) +
+        ':' + pad(this.getMinutes()) +
+        ':' + pad(this.getSeconds()) +
+        dif + pad(tzo / 60) +
+        ':' + pad(tzo % 60);
+}
