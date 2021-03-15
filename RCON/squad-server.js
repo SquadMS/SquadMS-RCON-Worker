1cbd16dce842907c319b35197e2c9023763007b0
Patrick Henninger <patrick.henninger@contagt.com>
2021-03-15 20:29:57 +0100

    Remove unused code, Simplfy KeepAlive,

--- RCON/squad-server.js
+++ RCON/squad-server.js
@@ -123,63 +123,13 @@ export default class SquadRcon extends Rcon {
     }
   }
 
-  async getCurrentMap() {
-    const response = await this.execute('ShowCurrentMap');
-    const match = response.match(/^Current level is (.*), layer is (.*)/);
-    return { level: match[1], layer: match[2] };
-  }
-
-  async getNextMap() {
-    const response = await this.execute('ShowNextMap');
-    const match = response.match(/^Next level is (.*), layer is (.*)/);
-    return {
-      level: match[1] !== '' ? match[1] : null,
-      layer: match[2] !== 'To be voted' ? match[2] : null
-    };
-  }
-
-  async getListPlayers() {
-    const response = await this.execute('ListPlayers');
-
-    const players = [];
-
-    for (const line of response.split('\n')) {
-      const match = line.match(
-        /ID: ([0-9]+) \| SteamID: ([0-9]{17}) \| Name: (.+) \| Team ID: ([0-9]+) \| Squad ID: ([0-9]+|N\/A)/
-      );
-      if (!match) continue;
-
-      players.push({
-        playerID: match[1],
-        steamID: match[2],
-        name: match[3],
-        teamID: match[4],
-        squadID: match[5] !== 'N/A' ? match[5] : null
-      });
-    }
-
-    return players;
-  }
-
-  async broadcast(message) {
-    await this.execute(`AdminBroadcast ${message}`);
-  }
-
-  async warn(steamID, message) {
-    await this.execute(`AdminWarn "${steamID}" ${message}`);
-  }
-
-  async switchTeam(steamID) {
-    await this.execute(`AdminForceTeamChange "${steamID}"`);
-  }
-
   async keepAlive() {
     if (this.keepAliveTimeout) clearTimeout(this.keepAliveTimeout);
 
     Logger.verbose('SquadRcon', 1, `Sending KeepAlive Command`);
 
     try {
-      this.players = (await this.getListPlayers())
+      await this.execute('ListPlayers');
     } catch (err) {
       Logger.verbose('SquadRcon', 1, 'Failed to keep alive.', err);
     }
