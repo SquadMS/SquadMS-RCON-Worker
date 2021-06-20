935e49a2974ec06ee2bfd98d8ab06af34c266240
Patrick <patrick.henninger@contagt.com>
2021-06-20 02:30:50 +0200

    Refactor toIsoString
    Add milliseconds / fraction seconds to toIsoString and refactor the method.

--- prototypes/Date.js
+++ prototypes/Date.js
@@ -1,17 +1,20 @@
 /* https://stackoverflow.com/questions/8083410/how-can-i-set-the-default-timezone-in-node-js */
 Date.prototype.toIsoString = function() {
-    var tzo = - this.getTimezoneOffset(),
-        dif = tzo >= 0 ? '+' : '-',
-        pad = function(num) {
-            var norm = Math.floor(Math.abs(num));
-            return (norm < 10 ? '0' : '') + norm;
-        };
+    const tzo = - this.getTimezoneOffset();
+    const dif = tzo >= 0 ? '+' : '-';
+    
+    const pad = function(num, digits = 2) {       
+        const norm = Math.floor(Math.abs(num)).toString();
+        return norm.padStart(digits, '0');
+    };
+    
     return this.getFullYear() +
         '-' + pad(this.getMonth() + 1) +
         '-' + pad(this.getDate()) +
         'T' + pad(this.getHours()) +
         ':' + pad(this.getMinutes()) +
         ':' + pad(this.getSeconds()) +
+        '.' + pad(this.getMilliseconds(), 3) +
         dif + pad(tzo / 60) +
         ':' + pad(tzo % 60);
 }
